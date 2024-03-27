edf7c5da2f03b124ce44d3fb3edf7181cb67c826
박준혁 <201921001@sangmyung.kr>
2024-03-28 00:37:01 +0900

    객체데이터를 props로 넘기기
    객체데이터를 props를 넘겨서 배열.키이름으로 접근하여 할당해줌

---
+++ 03 React Essentials/01-starting-project/Readme.md
@@ -0,0 +1,5 @@
+#유데미 강의 챕터 3
+
+1. component -> 컴포넌트 커스텀
+2. {} -> img , arr ->변수를 넣어 동적인 렌더링
+3. props를 이용한 component 재사용
--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -1,3 +1,6 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
+import { CORE_CONCEPTS } from "./data.js";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -5,25 +8,57 @@ function genRandomInt(max) {
 }
 
 function Header() {
-  const ind = genRandomInt(2);
+  const Description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[ind]} React concepts you will need for almost any app
-        you are going to build!
+        {Description} React concepts you will need for almost any app you are
+        going to build!
       </p>
     </header>
   );
 }
-
+function CoreConcepts({ title, img, description }) {
+  return (
+    <li>
+      <img src={img} alt="..." />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              img={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[1].title}
+              img={CORE_CONCEPTS[1].image}
+              description={CORE_CONCEPTS[1].description}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[2].title}
+              img={CORE_CONCEPTS[2].image}
+              description={CORE_CONCEPTS[2].description}
+            />
+            <CoreConcepts
+              title={CORE_CONCEPTS[3].title}
+              img={CORE_CONCEPTS[3].image}
+              description={CORE_CONCEPTS[3].description}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
