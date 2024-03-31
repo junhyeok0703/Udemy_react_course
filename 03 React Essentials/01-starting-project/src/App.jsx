f437f491ef05b41886913a46b42471c9a586881c
박준혁 <201921001@sangmyung.kr>
2024-03-31 19:55:24 +0900

    State가 null일 경우 초기 렌더링 오류(조건부 렌더링)
    State null일 경우 렌더링될때 state가 어떤 걸 선택해야되는지 모르는 오류가 발생하는데  그때 그냥 null일 경우로 예외처리 하여 조건부 렌더링을 사용하면 된다.

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -3,8 +3,9 @@ import Header from "./components/Header/Header.jsx";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
+import { EXAMPLES } from "./data.js";
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please select a topic.");
+  const [selectedTopic, setSelectedTopic] = useState("");
   const handleSelect = (c) => {
     setSelectedTopic(c);
     console.log(selectedTopic);
@@ -25,14 +26,24 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TaㅇㅇㅇㅇbButton onSelect={handleSelect}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Components
-            </TaㅇㅇㅇㅇbButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? (
+            <h3>Please select a topic </h3>
+          ) : (
+            <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic]?.title}</h3>
+              <p>{EXAMPLES[selectedTopic]?.description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTopic]?.code}</code>
+              </pre>
+            </div>
+          )}
         </section>
       </main>
     </div>
