eef2262c8b9fbdf17e90a60a6f9199d57e22fbec
박준혁 <201921001@sangmyung.kr>
2024-03-31 20:04:00 +0900

    조건부 렌더링 심화버전
    그냥 변수에다가 내용들을 넣고 그변수만 렌더링 하면 된다. 처음 고르지않았을때 null일 경우의 컨텐츠도 넣어서 보다 return 쪽에서 간단하게 코드를 쓸수있다.

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -10,6 +10,19 @@ function App() {
     setSelectedTopic(c);
     console.log(selectedTopic);
   };
+
+  let tabContent = <h3>Please select a topic </h3>;
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic]?.title}</h3>
+        <p>{EXAMPLES[selectedTopic]?.description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic]?.code}</code>
+        </pre>
+      </div>
+    );
+  }
   return (
     <div>
       <Header />
@@ -33,17 +46,7 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-          {!selectedTopic ? (
-            <h3>Please select a topic </h3>
-          ) : (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic]?.title}</h3>
-              <p>{EXAMPLES[selectedTopic]?.description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic]?.code}</code>
-              </pre>
-            </div>
-          )}
+          {tabContent}
         </section>
       </main>
     </div>
