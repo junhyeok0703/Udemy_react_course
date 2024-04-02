b7b5bcdfebfe596b36c43b404cec83b0a55c1930
박준혁 <201921001@sangmyung.kr>
2024-04-02 21:14:16 +0900

    map으로 데이터 동적으로 출력
    배열에서 map으로 html요소와 같이 넣고 key도 넣으면 데이터를 뿌릴수있게 된다. 코드의 양이 적어짐

--- 03 React Essentials/01-starting-project/src/App.jsx
+++ 03 React Essentials/01-starting-project/src/App.jsx
@@ -30,10 +30,12 @@ function App() {
         <section id="core-concepts">
           <h2>Time to get started!</h2>
           <ul>
-            <CoreConcepts {...CORE_CONCEPTS[0]} />
-            <CoreConcepts {...CORE_CONCEPTS[1]} />
-            <CoreConcepts {...CORE_CONCEPTS[2]} />
-            <CoreConcepts {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item, index) => (
+              <CoreConcepts key={index} {...item} />
+              // <CoreConcepts {...CORE_CONCEPTS[1]} />
+              //<CoreConcepts {...CORE_CONCEPTS[2]} />
+              //<CoreConcepts {...CORE_CONCEPTS[3]} />
+            ))}
           </ul>
         </section>
         <section id="examples">
